853d68d678d25d5cb1bd8499bbfecea5d9b9b646
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-27 11:24:51 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skillData = [
+  {
+    name: "HTML",
+    level: "intermediate",
+    color: "#f18247",
+  },
+  {
+    name: "CSS",
+    level: "advanced",
+    color: "#53a6e0",
+  },
+  {
+    name: "JavaScripts",
+    level: "Advanced",
+    color: "#F9D423",
+  },
+  {
+    name: "React",
+    level: "Intermediate",
+    color: "#61DAFB",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="IMG_2840.JPG" alt="Benny" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Benny Chrispin</h1>
+      <p>
+        <b>Full Stack Developer</b> And Student at Udemy
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  const programing = skillData;
+  return (
+    <>
+      {programing.map((list) => (
+        <li style={{ color: `${list.color}` }}>
+          {`${list.level}: ${list.name}`}{" "}
+        </li>
+      ))}
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
